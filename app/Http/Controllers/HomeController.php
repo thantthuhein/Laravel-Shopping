f8b730ac1b775b52078cb6b7ae33ba43b4af29ea
tth <thantthuhein97@gmail.com>
2019-03-13 07:30:26 +0630

    added auth scaffold

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
-use App\Order;
-use App\CreditpointsCard;
 
 class HomeController extends Controller
 {
@@ -26,90 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $user = User::find(auth()->user()->id);
-        $orders = $user->orders;
-        return view('/profile/page', ['user' => $user, 'orders' => $orders]);
-    }
-
-    public function admin()
-    {
-        $totalUsers = User::all();
-        $totalOrders = Order::all();
-        // dd($totalOrders);
-        $totalOrders->transform(function($totalOrder, $key) {
-            $totalOrder->cart = unserialize(base64_decode($totalOrder->cart));
-            return $totalOrder;
-        });
-
-        $totalPrice = 0;
-        foreach ($totalOrders as $order) {
-            $totalPrice += $order->cart->totalPrice;
-        }
-        // dd($totalPrice);
-        return view('/admin/dashboard',['totalUsers' => $totalUsers, 'totalOrders' => $totalOrders , 'totalPrice' => $totalPrice]);
-    }
-    
-    public function products()
-    {
-        $products = \App\Product::all();
-        return view('/admin/products', ['products' => $products]);
-    }
-
-    public function categories()
-    {
-        $categories = \App\Category::all();
-        return view('/admin/categories', ['categories' => $categories]);
-    }
-
-    public function users()
-    {
-        $users = \App\User::all();
-        return view('/admin/users', ['users' => $users]);
-    }
-
-    public function orders()
-    {
-        return view('/admin/orders');
-    }
-
-    public function showTotalOrders()
-    {
-        $orders = Order::latest()->get();
-        $orders->transform(function($order, $key) {
-            $order->cart = unserialize(base64_decode($order->cart));
-            return $order;
-        });
-        // foreach($orders as $order) {
-        //     dd($order->cart->items);
-        // }
-        return view('/admin/showTotalOrders', ['orders' => $orders]);
-    }
-
-    public function getDate(Request $request) 
-    {
-        // dd($request->all());
-        $time = strtotime($request->date);
-        $date = date('d: D : M : Y');
-        
-        $orders = \App\Order::whereDate('created_at', $request->date)->get();
-        // dd($orders);
-        $orders->transform(function($order, $key) {
-            $order->cart = unserialize(base64_decode($order->cart));
-            return $order;
-        });
-        $totalAmount = 0;
-        foreach($orders as $order) {
-            $totalAmount += $order->cart->totalPrice;
-        }
-        return view('/admin/orders', ['orders' => $orders, 'date' => $date, 'totalAmount' => $totalAmount]);
-    }
-
-    public function cardsDetails()
-    {
-        $totalCards = CreditpointsCard::all();
-        $useableCards = CreditpointsCard::where('useable', TRUE)->get();
-        $usedCards = CreditpointsCard::where('useable', FALSE)->get();
-
-        return view('/admin/creditcards', ['totalCards' => $totalCards, 'useableCards' => $useableCards, 'usedCards' => $usedCards]);
+        return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -103,3 +103,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
