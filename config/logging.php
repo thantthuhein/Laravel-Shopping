dba280745a00b0a3279699b2cbdf06d93a66de4f
tth <thantthuhein97@gmail.com>
2019-03-13 06:42:19 +0630

    use errorlog

--- config/logging.php
+++ config/logging.php
@@ -36,11 +36,11 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['single'],
         ],
 
         'single' => [
-            'driver' => 'single',
+            'driver' => 'errorlog',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
